4a915b8c84b2309dffd8fdcfc5327080362c431b
BilaGigante <50372366+BilaGigante@users.noreply.github.com>
2019-06-03 08:49:25 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1559562437,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ship-sheet0.png",
+		"images/firebutton-sheet0.png",
+		"images/myfire-sheet0.png",
+		"images/up-sheet0.png",
+		"images/roght-sheet0.png",
+		"images/ieft-sheet0.png",
+		"images/down-sheet0.png",
+		"images/background-sheet0.png",
+		"images/background-sheet1.png",
+		"images/enemy-sheet0.png",
+		"images/powerup-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
